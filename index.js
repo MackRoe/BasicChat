c1139c4f8ab34898bc6f99b80d6cbe0efccc7103
MackRoe <elaine.music@students.makeschool.com>
2020-11-18 20:11:11 -0500

    user messages now send to all users

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ io.on('connection', (socket) => {
   });
 });
 
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
